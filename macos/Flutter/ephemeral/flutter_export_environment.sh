82d2e87ae015165b42af85e2c92afaec36ef9a74
planck <goodsforexchange@gmail.com>
2025-10-01 14:42:01 +0300

    CHANGE_the icon and splash

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\goods\goods_suppliers"
+export "FLUTTER_APPLICATION_PATH=D:\goods\goods_suppliers"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
